bf53aaa7799e38c06b68bd907a2386972437d57e
Edgar Aparicio Baeza <holamundo@edgarbaeza.mx>
2022-11-09 20:06:24 -0600

    Compatibility with new hellpunch service v1

--- constants.js
+++ constants.js
@@ -23,15 +23,15 @@ const GAME_MODES = [
 ]
 
 const modosEn = {
-    '1': '1v1 Supremacy',
-    '2': 'Team Supremacy',
-    '3': 'Treaty',
+    '1vs1': '1v1 Supremacy',
+    'teamSupremacy': 'Team Supremacy',
+    'treaty': 'Treaty',
 }
 
 const modosEs = {
-    '1': 'Supremacía 1 vs 1',
-    '2': 'Supremacía en Equipo',
-    '3': 'Tratado',
+    '1vs1': 'Supremacía 1 vs 1',
+    'teamSupremacy': 'Supremacía en Equipo',
+    'treaty': 'Tratado',
 }
 
 module.exports = {
--- services/hellpunch.service.js
+++ services/hellpunch.service.js
@@ -1,8 +1,6 @@
 const axios = require("axios");
 const JSONbig = require('json-bigint')
 
-const url = (mode = '1vs1') => `https://decks.aoe3explorer.com/rest/v1/${mode}`
-
 const config = {
   headers: {
     'apikey': process.env.API_KEY,
@@ -11,42 +9,20 @@ const config = {
   transformResponse: [data  => data]
 };
 
-async function leaderboarSvc(searchValue = '', { modo = '1vs1', field = 'name', searchPlayer = false }) {
+async function leaderboarSvc(searchValue = '', { modo = '1vs1', field = 'name' }) {
   try {
-    config.url = url(modo)
+    const url = `${process.env.SERVICE_URL}${modo}`
     config.params['select'] = `*`
-    if (searchPlayer) {
-      config.params[`${field}`] = `ilike.%${searchValue}%`
-    } else {
-      config.params[`${field}`] = `ilike.%${searchValue}%`
-    }
+    config.params[`${field}`] = `ilike.%${searchValue}%`
     config.params['order'] = `rank`
-    const resp = await axios.get(url(modo), config);
-    if (!resp) return null
-/*TODO THE RESONPONSE NOW IS A LIST   
-Searching for doribalam yields this result
-[
-  {
-    "rank": 478,
-    "elo": 1421,
-    "name": "DoriBalam",
-    "clan": "MUNDO",
-    "region": 3,
-    "steamId": 2533274882505213,
-    "gameId": 5718291,
-    "internalNum": 1324567,
-    "wins": 649,
-    "losses": 663,
-    "drops": 4,
-    "streak": 4,
-    "lastOnline": "1668015539"
-  }
-]
+    
+    const resp = await axios.get(url, config);
+    
+    if (!resp.data) return null
 
-*/
-    //const players = JSONbig.parse(resp.data).documents;
+    const players = JSONbig.parse(resp.data);
 
-    return resp;
+    return players;
   } catch (error) {
     console.log(error);
     return null;
